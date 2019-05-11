d4c12d9be2a8e49d9d8bc2fed9461f60d426d3fc
Kirill Korolev <korkirik@gmail.com>
2019-05-11 03:15:26 +0200

    finished pvector commit

--- UnitTesting/pvector.py
+++ UnitTesting/pvector.py
@@ -1,44 +1,98 @@
-#Brief Rewritten from code in C
+#Brief Rewritten and Refactored from code in C
 #  - A toolbox that manipulate all the physics behind the simulation
-# Inherit
-#  - None
 # Detail
 # - Pvector class contains all the necessary tools for doing maths
 #   on a two-dimensional vector space. Most of the functions here
 #   to generate ideal forces to drive the agents.
+import math
 
 class Pvector:
-    x = 0.0
-    y = 0.0
 
     def __init__(self, xCoordinate, yCoordinate):
-        self.x = xCoordinate
-        self.y = yCoordinate
-
+        self.x = float(xCoordinate)
+        self.y = float(yCoordinate)
 
     def add(self, pvector):
-        self.x+= pvector.x
-        self.y+= pvector.y
-        return Pvector(self.x, self.y)
+        x = self.x + pvector.x
+        y = self.y + pvector.y
+        return Pvector(x, y)
 
     def substract(self, pvector):
-        self.x-= pvector.x
-        self.y-= pvector.y
-        return Pvector(self.x, self.y)
+        x = self.x - pvector.x
+        y = self.y - pvector.y
+        return Pvector(x, y)
 
-    def multiplyScalar(self, alpha):
-        self.x *= alpha
-        self.y *= alpha
-        return Pvector(self.x, self.y)
+    def multiply(self, alpha):
+        x = self.x * alpha
+        y = self.y * alpha
+        return Pvector(x, y)
 
-    def divideScalar(self, alpha):
+    def divide(self, alpha):
         if alpha != 0:
-            self.x = self.x/alpha
-            self.y = self.y/alpha
+            x = self.x/float(alpha)
+            y = self.y/float(alpha)
+            return Pvector(x, y)
+        else:
             return Pvector(self.x, self.y)
+            print('Division by zero')
+
+    def addToSelf(self, pvector):
+        self.x = self.x + pvector.x
+        self.y = self.y + pvector.y
+
+    def substractfromSelf(self, pvector):
+        self.x = self.x - pvector.x
+        self.y = self.y - pvector.y
+
+    def multiplySelfByScalar(self, alpha):
+        self.x = self.x * alpha
+        self.y = self.y * alpha
+
+    def divideSelfByScalar(self, alpha):
+        if alpha != 0:
+            self.x = self.x/float(alpha)
+            self.y = self.y/float(alpha)
         else:
             print('Division by zero')
-            Pvector(self.x, self.y)
 
     def magnitudeSquared(self):
         return self.x*self.x + self.y*self.y
+
+    def magnitude(self):
+        return math.sqrt(self.x*self.x + self.y*self.y)
+
+    def normalize(self):
+        mag = self.magnitude()
+        self.divideSelfByScalar(mag)
+
+    def get(self):
+        return self
+
+    def setMagnitude(self, x):
+        self.normalize()
+        self.multiplySelfByScalar(x)
+
+    def limitMagnitude(self, lim):
+        if self.magnitude() > lim:
+            self.setMagnitude(lim)
+
+    def dotProduct(self, vector):
+        x = self.x * vector.x
+        y = self.y * vector.y
+        return x + y
+
+    def angleBetween(self, vector):
+        angle = math.acosf(self.dotProduct(vector)/(self.magnitude() * vector.magnitude()))
+        return angle
+
+    def distanceBetween(self, vector):
+        return math.sqrt((self.x - vector.x)*(self.x - vector.x) + (self.y - vector.y)*(self.y - vector.y))
+
+    def getNormalPoint(self, b, p):
+        pa = p.substract(self)
+        ba = b.substract(self)
+        ba.normalize()
+        value = pa.dotProduct(ba)
+        ba.multiplySelfByScalar(value)
+        normalPoint = a.add(ba)
+        return normalPoint
--- UnitTesting/test_pvector.py
+++ UnitTesting/test_pvector.py
@@ -3,22 +3,33 @@ print('Pvector Test')
 
 a = Pvector(1,2)
 b = Pvector(2,4)
-alpha = 4
+alpha = 2
 print('a = ',a.x, a.y)
 print('b = ',b.x, b.y)
 
-c = a.add(b)
-print(c.x, c.y)
-d = a.substract(b)
-print(d.x, d.y)
-e = a.multiplyScalar(alpha)
-print(e.x, e.y)
-f = a.divideScalar(alpha)
-print(f.x,f.y)
+#c = a.add(b)
+#print(c.x, c.y)
+#d = a.substract(b)
+#print(d.x, d.y)
 
-f2 = a.divideScalar(0)
-print(f.x, f.y)
+#e = a.multiplyByScalar(alpha)
+#print(e.x, e.y)
+#f = a.divideByScalar(alpha)
+#print(f.x,f.y)
 
-h = a.magnitudeSquared();
-print(h)
+#f2 = a.divideByScalar(0)
+#print(f2.x, f2.y)
+
+#h = a.magnitudeSquared()
+#i = a.magnitude()
+#print(h)
+#print(i)
+#print(a.x, a.y)
+#a.normalize()
+q = a.get()
+a.setMagnitude(5)
+q.limitMagnitude(4)
+print(q.x, q.y)
+print(q.magnitude())
+
 input('Press enter to close')
