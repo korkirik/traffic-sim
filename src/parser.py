b9b66764de4d2ec049f0d912ca51f639929b5936
Leen <leen.nijim@hsrw.org>
2019-06-19 01:51:30 +0200

    Parser changes 2.0

--- src/parser.py
+++ src/parser.py
@@ -4,20 +4,40 @@ with open('export_map_data.json', 'r') as export:
     meta_data = json.load(export)
 
 
-number_of_elements = 5 #len(meta['features'])
+number_of_elements = 5#len(meta_data['features'])
 
 for i in range(0, number_of_elements):
     geometry = meta_data['features'][i]['geometry']
     if geometry['type'] == 'LineString':
         properties = meta_data['features'][i]['properties']
-        highway = properties['highway']
+
+        highway = properties.get('highway','Street')
         #print(type(highway))
         print(highway)
-        name = properties['name']
+        name = properties.get('name','Street name')
+
         print(name)
-        lanes = properties['lanes']
+        lanes = int(properties.get('lanes','1'))
+
         print(lanes)
+        #print(type(lanes))
         print("\n")
+
+        startXcoor = geometry['coordinates'][0][0]
+        print(startXcoor)
+        print(type(startXcoor))
+
+        startYcoor = geometry['coordinates'][0][1]
+        print(startYcoor)
+        print(type(startYcoor))
+
+        endXcoor = geometry['coordinates'][-1][0]
+        print(endXcoor)
+        print(type(endXcoor))
+
+        endYcoor = geometry['coordinates'][-1][1]
+        print(endYcoor)
+        print(type(endYcoor))
 
 
 export.close()
