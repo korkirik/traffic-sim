f19e11649d265783df927ac85e187f90cc083dc5
Kirill Korolev <korkirik@gmail.com>
2019-07-03 02:20:02 +0200

    nodes merged and know connected nodes, map draws properly nodes

--- src/map.py
+++ src/map.py
@@ -2,7 +2,7 @@ from streetsegment import *
 from node import *
 from pvector import *
 from bokeh.plotting import figure, output_file, show
-output_file("map_build_0.1.2.html")
+output_file("map_build_0.2.1.html")
 
 class Map:
     def __init__(self):
@@ -20,7 +20,7 @@ class Map:
                 self.streetSegmentList[street_index].startPoint.x,
                 self.streetSegmentList[street_index].startPoint.y,
                 self.nodeCounter))
-            self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
+            #self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
             self.nodeList[self.nodeCounter].connectedNodesId.append(self.nodeCounter + 1)
             self.nodeCounter += 1
 
@@ -28,9 +28,12 @@ class Map:
                 self.streetSegmentList[street_index].endPoint.x,
                 self.streetSegmentList[street_index].endPoint.y,
                 self.nodeCounter))
-            self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
+            #self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
             self.nodeList[self.nodeCounter].connectedNodesId.append(self.nodeCounter - 1)
             self.nodeCounter += 1
+
+            self.nodeList[self.nodeCounter - 2].connectedNodes.append(self.nodeList[self.nodeCounter - 1])
+            self.nodeList[self.nodeCounter - 1].connectedNodes.append(self.nodeList[self.nodeCounter - 2])
 
     def mergeNodes(self):
         rem = []
@@ -38,6 +41,8 @@ class Map:
             for j in range(i + 1, len(self.nodeList)):
                     if self.nodeList[i].position.x == self.nodeList[j].position.x:
                         if self.nodeList[i].position.y == self.nodeList[j].position.y:
+                            self.nodeList[i].connectedNodesId = self.nodeList[i].connectedNodesId + self.nodeList[j].connectedNodesId
+                            self.nodeList[i].connectedNodes = self.nodeList[i].connectedNodes + self.nodeList[j].connectedNodes
                             rem.append(self.nodeList[j].nodeId)
 
         print('Nodes')
@@ -52,29 +57,29 @@ class Map:
         for indexes_to_delete in mer:
             for obj in self.nodeList:
                 if(obj.nodeId == indexes_to_delete):
-                    
+
                     self.nodeList.remove(obj)
                     self.deleted += 1
                     break
 
         print('Objects removed')
         print(self.deleted)
+        print(self.nodeList[15].connectedNodes)
 
     def printNodesStats(self):
         print('Nodes created:')
         print(self.nodeCounter)
         print('Current number of nodes')
         nodesNumber = len(self.nodeList)
-    #    for index in range(0,nodesNumber, 1):
-    #        print(self.nodeList[index].position.x, self.nodeList[index].position.y, self.nodeList[index].nodeId)
+
 
     def drawStreets(self):
         p = figure(plot_width=700, plot_height=700)
-        length = len(self.streetSegmentList)
-    #    for index in range(0,length,1):
-        #    x = [self.streetSegmentList[index].startPoint.x, self.streetSegmentList[index].endPoint.x]
-        #    y = [self.streetSegmentList[index].startPoint.y,self.streetSegmentList[index].endPoint.y]
-        #    p.line(x, y, line_width=2)
+        for node in self.nodeList:
+            for connected_node in node.connectedNodes:
+                x = [node.position.x, connected_node.position.x]
+                y = [node.position.y, connected_node.position.y]
+                p.line(x, y, line_width=2)
 
         nodesNumber = len(self.nodeList)
         for index in range(0,nodesNumber, 1):
--- src/node.py
+++ src/node.py
@@ -3,5 +3,5 @@ class Node:
     def __init__(self, x, y, nodeId):
         self.position = Pvector(x,y)
         self.nodeId = nodeId
-        self.connectedStreetSegments = list()
         self.connectedNodesId = list()
+        self.connectedNodes = list()
