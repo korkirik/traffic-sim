b4f125ba7619796ce56ea8201cc0c0f43684bb78
Kirill Korolev <korkirik@gmail.com>
2019-07-26 19:54:41 +0200

    Node Merger fixed. Now it functions as intended.

--- src/map.py
+++ src/map.py
@@ -21,7 +21,6 @@ class Map:
                 self.streetSegmentList[street_index].startPoint.y,
                 self.nodeCounter))
             #self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
-            self.nodeList[self.nodeCounter].connectedNodesId.append(self.nodeCounter + 1) #REMOVE THIS
             self.nodeCounter += 1
 
             self.nodeList.append(Node(
@@ -29,11 +28,11 @@ class Map:
                 self.streetSegmentList[street_index].endPoint.y,
                 self.nodeCounter))
             #self.nodeList[self.nodeCounter].connectedStreetSegments.append(self.streetSegmentList[street_index].name)
-            self.nodeList[self.nodeCounter].connectedNodesId.append(self.nodeCounter - 1) #REMOVE THIS
-            self.nodeCounter += 1
 
-            self.nodeList[self.nodeCounter - 2].connectedNodes.append(self.nodeList[self.nodeCounter - 1])
-            self.nodeList[self.nodeCounter - 1].connectedNodes.append(self.nodeList[self.nodeCounter - 2])
+            self.nodeList[self.nodeCounter - 1].connectedNodes.append(self.nodeList[self.nodeCounter])
+            self.nodeList[self.nodeCounter].connectedNodes.append(self.nodeList[self.nodeCounter - 1])
+
+            self.nodeCounter += 1
 
     def mergeNodes(self):
         rem = []
@@ -41,43 +40,38 @@ class Map:
             for j in range(i + 1, len(self.nodeList)):
                     if self.nodeList[i].position.x == self.nodeList[j].position.x:
                         if self.nodeList[i].position.y == self.nodeList[j].position.y:
-                            self.nodeList[i].connectedNodesId = self.nodeList[i].connectedNodesId + self.nodeList[j].connectedNodesId
                             self.nodeList[i].connectedNodes = self.nodeList[i].connectedNodes + self.nodeList[j].connectedNodes
+                            self.nodeList[i].merged = True
 
-                            #risk to get out of bound
-# removing duplicate from next node ! ALLO
-                            self.nodeList[j+1].removeNodeWithId(j)
-                            self.nodeList[j+1].connectedNodes.append(self.nodeList[i]) #linking next node to merged
-
+                            self.nodeList[j].removeConnectionsToMe()    #removing links to nodes marked for deletion
                             rem.append(self.nodeList[j].nodeId)
 
-        print('Nodes')
-        print(len(self.nodeList))
+        print('Nodes: ', len(self.nodeList))
+        print('Indexes found', len(rem))
+
         mer = sorted(rem, reverse = True)
-        print('Indexes found')
-        print(len(mer))
-        #print(self.nodeList)
         self.nodeList.reverse()
-        #del self.nodeList[mer[0]]
+        #removing Nodes that repeat
         self.deleted = 0
         for indexes_to_delete in mer:
             for obj in self.nodeList:
                 if(obj.nodeId == indexes_to_delete):
-                    #need to test more
-
+                                                    #need to test more
                     self.nodeList.remove(obj)
                     self.deleted += 1
                     break
 
-        print('Objects removed')
-        print(self.deleted)
+        print('Objects removed:', self.deleted)
+
+        #Now Linking Nodes back to new merged Nodes
+        for obj in self.nodeList:
+            if(obj.merged):
+                obj.addConnectionsToMe()
 
     def printNodesStats(self):
 
         for i in range(0,len(self.nodeList)):
-            print('Node id:')
-            print(self.nodeList[i].nodeId)
-            print('Adjacent Nodes:')
+            print('Node:', self.nodeList[i].nodeId, 'Adjacent Nodes:')
             for j in range(0,len(self.nodeList[i].connectedNodes)):
                 print(self.nodeList[i].connectedNodes[j].nodeId)
 
--- src/node.py
+++ src/node.py
@@ -3,12 +3,29 @@ class Node:
     def __init__(self, x, y, nodeId):
         self.position = Pvector(x,y)
         self.nodeId = nodeId
-        self.connectedNodesId = list()
         self.connectedNodes = list()
+        self.merged = False
 
-    def removeNodeWithId(self, index_to_delete):
+    def removeConnectionsToMe(self):
+        for obj in self.connectedNodes:
+            for o in obj.connectedNodes:
+                if(o.nodeId == self.nodeId):
+                    obj.connectedNodes.remove(o)
+                    break
+
+    def addConnectionsToMe(self):
+        for obj in self.connectedNodes:
+            foundFlag = False
+            for o in obj.connectedNodes:
+                if(o.nodeId == self.nodeId):
+                    foundFlag = True
+                    break                   #found id in list
+            if(not foundFlag):
+                obj.connectedNodes.append(self)
+
+
+    def removeConnectedNodeWithId(self, index_to_delete):
         for obj in self.connectedNodes:
             if(obj.nodeId == index_to_delete):
                 self.connectedNodes.remove(obj)
-                #remove ID
                 break
